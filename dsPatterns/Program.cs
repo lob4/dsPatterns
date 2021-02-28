54397e5b120a14826c54129d0a561d96ce679817
slawekCL <slawek@cledar.pl>
2021-02-28 13:55:05 +0100

    setup - add main run program

---
+++ dsPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dsPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Design Patterns!");
+        }
+    }
+}
